df384d57bf75d2cb8b0c53a4d78037e43d47ec79
Gustavo Monte <gmonte@tutanota.com>
2023-10-01 18:27:52 -0300

    grasshoperMessage

---
+++ typescript/kyu8/grasshopperMessage.ts
@@ -0,0 +1,13 @@
+// Grass hopper personalized message
+
+// DESCRIPTION:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+export const greet = (name: string, owner: string): string =>
+  name === owner ? "Hello boss" : "Hello guest";
