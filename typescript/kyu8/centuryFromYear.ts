08a30fc9e3f0f17857f2c7d8c6b540a7c241144b
Gustavo Monte <gmonte@tutanota.com>
2023-10-06 01:54:44 -0300

    centuryFromYear

---
+++ typescript/kyu8/centuryFromYear.ts
@@ -0,0 +1,23 @@
+// ntroduction
+// The first century spans from the **year 1** up to *and including* the year 100, the second century - from the year 101 up to *and including* the year 200, etc.
+// Task
+// Given a year, return the century it is in.
+// Examples
+// `1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20`
+// Note: this kata uses strict construction as shown in the description and the examples, you can read more about it ~[here](https://en.wikipedia.org/wiki/Century)~
+export const centuryFromYear = (year: number): number => {
+  const [firstDigit, secondDigit] = year.toString();
+
+  let result: number = 0;
+  if (year % 10 === 0) {
+    result = parseInt(firstDigit + secondDigit);
+  }
+  if (year % 10 !== 0) {
+    result = parseInt(firstDigit + secondDigit) + 1;
+  }
+
+  return result;
+};
