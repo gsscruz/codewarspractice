50f6baa403aa27437837be9136330625ebfa6614
Gustavo Monte <gmonte@tutanota.com>
2023-10-20 03:46:59 -0300

    countBeads.ts

--- typescript/kyu7/countBeads.ts
+++ typescript/kyu7/countBeads.ts
@@ -5,40 +5,43 @@
 // Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.If there are less than 2 blue beads return 0.
 
 export function countRedBeads(n: number): number {
-  if (n < 4) return 0;
+  //anwser for current problem; where you receive *N BLUE* beads and count the red ones.
 
-  const beadsList: string[] = [];
+  return n < 2 ? 0 : n * 2 - 2;
 
-  for (let i: number = 0; i < n; i++) {
-    beadsList.push("🧿");
-  }
+  //anwser for problem where you are given *N* beads and count the red ones.
 
-  const sortedBeadsList: string[][] = [];
+  //   const beadsList: string[] = [];
 
-  function countThreeBeads() {
-    sortedBeadsList.push(["B", "R", "R"]);
-  }
-  function countTwoBeads() {
-    sortedBeadsList.push(["B", "R"]);
-  }
+  //   for (let i: number = 0; i < n; i++) {
+  //     beadsList.push("🧿");
+  //   }
 
-  let unsortedBeadsList = [...beadsList];
+  //   const sortedBeadsList: string[][] = [];
 
-  for (let i = 0; i < beadsList.length; i++) {
-    if (unsortedBeadsList.length >= 3) {
-      if (i % 3 === 0) {
-        countThreeBeads();
-        unsortedBeadsList = unsortedBeadsList.slice(3);
-      }
-    }
+  //   function countThreeBeads() {
+  //     sortedBeadsList.push(["B", "R", "R"]);
+  //   }
+  //   function countTwoBeads() {
+  //     sortedBeadsList.push(["B", "R"]);
+  //   }
 
-    if (unsortedBeadsList.length >= 2 && unsortedBeadsList.length < 3) {
-      countTwoBeads();
-      unsortedBeadsList = unsortedBeadsList.slice(2);
-    }
-  }
+  //   let unsortedBeadsList = [...beadsList];
 
-  return sortedBeadsList.flat().filter((bead) => bead === "R").length;
+  //   for (let i = 0; i < beadsList.length; i++) {
+  //     if (unsortedBeadsList.length >= 3) {
+  //       if (i % 3 === 0) {
+  //         countThreeBeads();
+  //         unsortedBeadsList = unsortedBeadsList.slice(3);
+  //       }
+  //     }
+
+  //     if (unsortedBeadsList.length >= 2 && unsortedBeadsList.length < 3) {
+  //       countTwoBeads();
+  //       unsortedBeadsList = unsortedBeadsList.slice(2);
+  //     }
+  //   }
+
+  //   return sortedBeadsList.flat().filter((bead) => bead === "R").length;
 }
-
-countRedBeads(8);
+countRedBeads(3);
